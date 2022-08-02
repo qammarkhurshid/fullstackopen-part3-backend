5b7679b8d87983f74097c8cd4da93f2745e94b4e
qammarkhurshid <qamar.khurshid@gmail.com>
2022-08-02 23:46:38 +0500

    Ex:3.13-3.14

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Phonebook = require('./models/phonebook');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const SOME = process.env.TEST_VAR;
 
 app.use(cors());
 app.use(express.json());
@@ -29,66 +29,40 @@ app.use(
   })
 );
 
-const persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-const generateId = () => {
-  return Math.floor(Math.random() * (100000 - 1 + 1) + 1);
+const checkDuplicate = async (name) => {
+  const persons = await Phonebook.find({ name });
+  return persons.length !== 0;
 };
 
-const checkDuplicate = (name) => {
-  return persons.find((p) => p.name.toLowerCase() === name.toLowerCase());
-};
-
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', async (req, res) => {
   const { body } = req;
   if (!body.name || !body.number) {
     return res.status(400).send({ error: 'Name and Number is required' });
   }
-  if (checkDuplicate(body.name)) {
-    return res.status(400).send({ error: 'Name must be unique' });
-  }
-  const personId = generateId();
-  body.id = personId;
-  persons.push(body);
-  return res.status(201).send(persons);
+  const person = new Phonebook({
+    name: body.name,
+    number: body.number,
+  });
+  await person.save();
+  return res.status(201).send(person);
 });
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', async (req, res) => {
+  const persons = await Phonebook.find({});
   return res.status(200).json(persons);
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const person = persons.find((p) => p.id === +req.params.id);
+app.get('/api/persons/:id', async (req, res) => {
+  const person = await Phonebook.findOne({ _id: req.params.id });
   if (!person) return res.status(404).end();
   return res.status(200).json(person);
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const deletionIdx = persons.findIndex((p) => p.id === +req.params.id);
-  if (deletionIdx > -1) {
-    persons.splice(deletionIdx, 1);
-    return res.status(200).json(persons);
+app.delete('/api/persons/:id', async (req, res) => {
+  const person = await Phonebook.findOne({ _id: req.params.id });
+  if (person) {
+    await Phonebook.findOneAndDelete({ _id: req.params.id });
+    return res.status(200).json(person);
   }
   return res.status(204).end();
 });
